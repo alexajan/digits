cef6cfc87bfffd244616baf9ad9b4fc958ec86e8
Alexa Jan <ajan@mit.edu>
2018-02-28 11:28:33 -0500

    implemented digits tutorial

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,12 +2,9 @@ from digits import example
 
 import unittest
 
-class SampleTestSuite(unittest.TestCase):
-    """Basic test cases."""
+class SoftmaxTestSuite(unittest.TestCase):
+    """Make sure softmax on MNIST data has the expected accuracy. (0.91 <= accuracy <= 0.93)"""
 
-    def test_example(self):
-        assert example.tensorflow_example() == [[3]]
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_softmax_digits(self):
+        result = example.softmax_digits()
+        assert result >= 0.91 and result <=0.93
